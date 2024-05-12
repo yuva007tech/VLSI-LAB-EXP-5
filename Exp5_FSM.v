869e0ec01a2ced1069075c1f1f0307557bbbeba8
yuva007tech <123287599+yuva007tech@users.noreply.github.com>
2024-05-12 22:49:09 +0530

    Update Exp5_FSM.v

--- Exp5_FSM.v
+++ Exp5_FSM.v
@@ -1,17 +1,53 @@
-module FSM_moore(clk, rst, x, z);
-input clk, rst, x;
-output z;
-reg [2:1] present_state, NEXT_STATE; 
-parameter S0=2'b00, S1=2'b01, S2=2'b10, S3=2'b11;
-// define the next state combinational circuit
-always@(x,present_state)
-case(present_state)
-
+module Sequence_Detector_Moore(clock,reset,sequence_in,detector_out);
+input clock, reset, sequence_in; 
+output reg detector_out; 
+parameter  S0=2'b00,S1=2'b01,S2=2'b10,S3=2'b11;
+reg [1:0] current_state, next_state; 
+always @(posedge clock, posedge reset)
+begin
+ if(reset==1) 
+ current_state <= S0;
+ else
+ current_state <= next_state; 
+end 
+always @(current_state,sequence_in)
+begin
+ case(current_state) 
+ 	S0:begin
+		if(sequence_in==1)
+   			next_state = S1;
+  		else
+   			next_state = S0;
+ 	   end
+ 	S1:begin
+  		if(sequence_in==0)
+   			next_state = S2;
+  		else
+   			next_state = S1;
+ 	   end
+  S2:begin
+  	if(sequence_in==0)
+   		next_state = S0;
+ 	 else
+   		next_state = S3;
+    end 
+  S3:begin
+  	if(sequence_in==0)
+   		next_state = S0;
+  	else
+   		next_state = S1;
+     end
+	default:next_state = S0;
 endcase
-//define the sequential block
-always@(negedge rst, posedge clk)
-
-
-
-assign z=(present_state==S3); // define output
+end
+always @(current_state)
+begin 
+ case(current_state) 
+ 	S0:   detector_out = 0;
+ 	S1:   detector_out = 0;
+ 	S2:  detector_out = 0;
+ 	S3:  detector_out = 1;
+ 	default:  detector_out = 0;
+ endcase
+end 
 endmodule
